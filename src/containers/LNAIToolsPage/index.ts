2c5328066925b117528eee7b54cb39f3d059a56d
Ashish Yadav <savitayadavmansarover@gmail.com>
2023-11-21 17:40:58 +0530

    updated import export of tool kit r

---
+++ src/containers/LNAIToolsPage/index.ts
@@ -0,0 +1,29 @@
+import BussinessSetup from "./BussinessSetup/BussinessSetup";
+import EpfCalculator from "./EpfCalculator/EpfCalculator";
+import GratuityCalculator from "./GratuitCalculator/GratuityCalculator";
+import GSTcalculator from "./GSTcalculator/GSTcalculator";
+import HRAcalculator from "./HRAcalculator/HRAcalculator";
+import IncomeTaxCalculator from "./IncomeTaxCalculator/IncomeTaxCalculator";
+import NpsCalculator from "./NpsCalculator/NpsCalculator";
+import PPFcalculator from "./PPFcalculator/PPFcalculator";
+import RDcalculator from "./RDcalculator/RDcalculator";
+import RetirementPlanning from "./RetirementPlanning/RetirementPlanning";
+import SalaryCalculator from "./SalaryCalculator/SalaryCalculator";
+import SIPcalculator from "./SIPcalculator/SIPcalculator";
+import TdsCalculator from "./TdsCalculator/TdsCalculator";
+
+export {
+    BussinessSetup,
+    EpfCalculator,
+    GratuityCalculator,
+    GSTcalculator,
+    HRAcalculator,
+    IncomeTaxCalculator,
+    NpsCalculator,
+    PPFcalculator,
+    RDcalculator,
+    RetirementPlanning,
+    SalaryCalculator,
+    SIPcalculator,
+    TdsCalculator,
+};
