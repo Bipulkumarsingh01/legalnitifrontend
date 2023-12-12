0743222716cf174505ddc0022ba55b3553904896
Ashish Yadav <savitayadavmansarover@gmail.com>
2023-12-12 14:43:29 +0530

    done with ai services animation

--- src/components/LNAILandingView/LNAIServiceInfo/LNAIServiceInfo.tsx
+++ src/components/LNAILandingView/LNAIServiceInfo/LNAIServiceInfo.tsx
@@ -41,142 +41,135 @@ const LNAIServiceInfo = () => {
         </span>{" "}
         over one platform
       </h1>
-      <div className="lnai-service-info-cards-main-container-1">
-        <div className="lnai-service-info-card">
-          <img
-            src={ImportLicense}
-            alt="Import License"
-            height={75}
-            width={75}
-          />
-          <p>Import License</p>
+      <div className="outer-slide-div-legalserices">
+        <div className="lnai-service-info-cards-main-container-1 ">
+          <div className="lnai-service-info-card">
+            <img
+              src={ImportLicense}
+              alt="Import License"
+              height={75}
+              width={75}
+            />
+            <p>Import License</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={PartnershipDeals}
+              alt="Import License"
+              height={75}
+              width={75}
+            />
+            <p>Partnership Deals</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={LegalDrafting}
+              alt="Import License"
+              height={75}
+              width={75}
+            />
+            <p>Legal Drafting</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Affidavit} alt="Import License" height={75} width={75} />
+            <p>Affidavit</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Will} alt="Import License" height={75} width={75} />
+            <p>Will</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={Company_taxAndCompilance}
+              alt="Changes Pvt. Ltd"
+              height={75}
+              width={75}
+            />
+            <p>Changes in Pvt. Limited Company</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Accounting} alt="Accounting" height={75} width={75} />
+            <p>GST & other Indirect Tax</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={ImportLicense}
+              alt="ImportLicense"
+              height={75}
+              width={75}
+            />
+            <p>Changes in LLP</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Design} alt="Design" height={75} width={75} />
+            <p>Design Registration</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={SigningADocument}
+              alt="SigningADocument"
+              height={75}
+              width={75}
+            />
+            <p>Business Contracts</p>
+          </div>
         </div>
-
-        <div className="lnai-service-info-card">
-          <img
-            src={PartnershipDeals}
-            alt="Import License"
-            height={75}
-            width={75}
-          />
-          <p>Partnership Deals</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img
-            src={LegalDrafting}
-            alt="Import License"
-            height={75}
-            width={75}
-          />
-          <p>Legal Drafting</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Affidavit} alt="Import License" height={75} width={75} />
-          <p>Affidavit</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Will} alt="Import License" height={75} width={75} />
-          <p>Will</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img
-            src={Company_taxAndCompilance}
-            alt="Changes Pvt. Ltd"
-            height={75}
-            width={75}
-          />
-          <p>Changes in Pvt. Limited Company</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Accounting} alt="Accounting" height={75} width={75} />
-          <p>GST & other Indirect Tax</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={ImportLicense} alt="ImportLicense" height={75} width={75} />
-          <p>Changes in LLP</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Design} alt="Design" height={75} width={75} />
-          <p>Design Registration</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img
-            src={SigningADocument}
-            alt="SigningADocument"
-            height={75}
-            width={75}
-          />
-          <p>Business Contracts</p>
-        </div>
-      </div>
-
-      <div className="lnai-service-info-cards-main-container-2">
-        <div className="lnai-service-info-card">
-          <img
-            src={MandatoryAnnualFilling}
-            alt="MandatoryAnnualFilling"
-            height={75}
-            width={75}
-          />
-          <p>Mandatory Annual Filling</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={HardWorking} alt="HardWorking" height={75} width={75} />
-          <p>Labour Compliance</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Estimate} alt="Estimate" height={75} width={75} />
-          <p>Accounting & Tax</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img
-            src={Business_taxAndCompilance}
-            alt="Business_taxAndCompilance"
-            height={75}
-            width={75}
-          />
-          <p>Convert you Business</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Concept} alt="Concept" height={75} width={75} />
-          <p>Patent</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Documents} alt="Documents" height={75} width={75} />
-          <p>Infringement</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Internet} alt="Internet" height={75} width={75} />
-          <p>Website Policies</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={PrivacyPolicy} alt="PrivacyPolicy" height={75} width={75} />
-          <p>HR Policies</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={RealEstate} alt="RealEstate" height={75} width={75} />
-          <p>Real Estate</p>
-        </div>
-
-        <div className="lnai-service-info-card">
-          <img src={Leave} alt="Leave" height={75} width={75} />
-          <p>Notice</p>
+        <div className="lnai-service-info-cards-main-container-2 ">
+          <div className="lnai-service-info-card">
+            <img
+              src={MandatoryAnnualFilling}
+              alt="MandatoryAnnualFilling"
+              height={75}
+              width={75}
+            />
+            <p>Mandatory Annual Filling</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={HardWorking} alt="HardWorking" height={75} width={75} />
+            <p>Labour Compliance</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Estimate} alt="Estimate" height={75} width={75} />
+            <p>Accounting & Tax</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={Business_taxAndCompilance}
+              alt="Business_taxAndCompilance"
+              height={75}
+              width={75}
+            />
+            <p>Convert you Business</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Concept} alt="Concept" height={75} width={75} />
+            <p>Patent</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Documents} alt="Documents" height={75} width={75} />
+            <p>Infringement</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Internet} alt="Internet" height={75} width={75} />
+            <p>Website Policies</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img
+              src={PrivacyPolicy}
+              alt="PrivacyPolicy"
+              height={75}
+              width={75}
+            />
+            <p>HR Policies</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={RealEstate} alt="RealEstate" height={75} width={75} />
+            <p>Real Estate</p>
+          </div>
+          <div className="lnai-service-info-card">
+            <img src={Leave} alt="Leave" height={75} width={75} />
+            <p>Notice</p>
+          </div>
         </div>
       </div>
     </div>
