a811c9fffa4adf4b65c5f9d0320c7ec04fb18fee
Koushik <koushikhr1441@gmail.com>
2023-12-15 12:34:14 +0530

    added docker configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // plugins: [react()],
+  // server: {
+  //   host: "localhost",
+  //   port: 3000,
+  // },
+
   plugins: [react()],
   server: {
-    host: "localhost",
-    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 3000, // you can replace this port with any port
   },
 });
